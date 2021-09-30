5e35959f5ec91c9a9a4611d3c48a9d706eb2814c
Mehrnaz-zand <zand.mehrnaz@gmail.com>
2021-09-30 16:45:27 +0200

    Added JS for search bar

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 
-
-let apiKey = `19351561bdce0a99202ae9e49984792f`
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&units=metric`
-
 function getDate(timestamp){
     let date = new Date(timestamp);
     let hours = date.getHours();
@@ -36,9 +32,23 @@ document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed)
 document.querySelector("#date").innerHTML = getDate(response.data.dt*1000);
 document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
-console.log(response.data);
 }
 
+function search(city) {
+let apiKey = `19351561bdce0a99202ae9e49984792f`
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInput = document.querySelector("#city-input");
+    search(cityInput.value);
+}
 
 
+
+let form = document.querySelector("#search-bar");
+form.addEventListener("submit", handleSubmit);
+
+search("tehran");
